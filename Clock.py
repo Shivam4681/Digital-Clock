c2e75b0ba9ff9fc607f67c025ac6a183ec169ef9
SHIVAM ROKADE <107171991+Shivam4681@users.noreply.github.com>
2023-06-22 19:49:58 +0530

    Add files via upload

---
+++ Clock.py
@@ -0,0 +1,23 @@
+                        #  Task 2 :- Make A Digital Clock
+
+
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title('Clock')
+
+def time():
+	string = strftime('%H:%M:%S %p')
+	lbl.config(text=string)
+	lbl.after(1000, time)
+
+
+lbl = Label(root, font=('calibri', 40, 'bold'),background='black',foreground='cyan')
+
+lbl.pack(anchor='center')
+time()
+
+mainloop()
